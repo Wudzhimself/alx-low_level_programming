4fc9495f0271909701b8400f536c9efa4f307dd9
Wudzhimself <seli.sedo@gmail>
2022-04-18 03:06:28 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -14,11 +14,10 @@ printf("infinite loop incoming :(\n");
 i = 0;
 
  /*
-  *while (i < 10)
-  *{
-  *Infinite loop - No increase of variable
-  *putchar(i) ;
-  *}
+  * while (i < 10)
+  * {
+  * putchar(i);
+  * }
   */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
